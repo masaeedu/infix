971eb1b2851bb2d8944aca75e2be94e8443a7ff2
Asad Saeeduddin <masaeedu@gmail.com>
2018-12-19 10:42:11 -0500

    Remove unnecessary abstractions

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const Fn = {
-  flip: f => x => y => f(y)(x),
-  pipe: fs => x => fs.reduce((p, f) => f(p), x),
   uncurry: f => args => args.reduce((p, c) => p(c), f)
 };
 const Obj = {
@@ -10,7 +8,7 @@ const Obj = {
 const uncurry = Symbol("@masaeedu/infix/uncurry");
 const infix = X => {
   const wrap = x => {
-    const methods = Obj.map(f => Fn.pipe([Fn.flip(f)(x), wrap]))(X);
+    const methods = Obj.map(f => v => wrap(f(v)(x)))(X);
     const uncurrier = k => xs => wrap(Fn.uncurry(X[k])([...xs, x]));
     return { ...methods, [uncurry]: uncurrier, unwrap: x };
   };
